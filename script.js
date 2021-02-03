97ea889cc4350631cab6bce3c2b802b6bdffc24f
Gui Guy <jedandeganwang1999@gmail.com>
2021-02-02 23:01:08 -0600

    Preparing for more peeps

--- script.js
+++ script.js
@@ -105,6 +105,8 @@ document.getElementById("back").addEventListener("click", function() {
 searchBox.addEventListener("input", function() {
 	if (searchWait !== null) clearTimeout(searchWait);
 	searchWait = setTimeout(function() {
+		if(!searchBox.value) return;
+		
 		const lev = new LevComparator(searchBox.value);
 		console.log(Object.keys(data).sort(function(a, b) {
 			return lev.compare(a, b);
